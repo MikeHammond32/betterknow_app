a3ee1fe3adfc2e91557260624f24b66fd84f6082
Mike <mikehammond32@gmail.com>
2019-07-31 21:38:38 -0400

    created restful create action for user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::UsersController < ApplicationController
-  def index
-    @contacts = Contact.all
-    if contact
+def create
+  @user = User.new(
+  {
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    phone_number: params[:phone_number],
+    location: params[:location],
+    bio: params[:bio]
+    })
+  if @user.save
+    render 'create.json.jb'
+  else
+    render 'error.json.jb'
+  end
 end
