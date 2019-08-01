3c57432d0608ada0d23b97b81586bbe972804e8e
Mike <mikehammond32@gmail.com>
2019-07-31 22:12:24 -0400

    created show, update , and create routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+    get '/users' => 'users#show'
+    patch '/users/:id' => 'user#update'
+    post '/users' => 'user#create'
+
 end
