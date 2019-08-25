cf068f822c2c61b09f521d57f26c884646b4ce74
Mike <mikehammond32@gmail.com>
2019-08-25 14:53:19 -0400

    backend account authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
+
+  def current_Account
+    auth_headers = request.headers['Authorization']
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: 'HS256' }
+        )
+        Account.find_by(id: decoded_token[0]["user_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  helper_method :current_Account
+
+  def authenticate_Account
+    unless current_Account
+      render json: {}, status: :unauthorized
+    end
+  end
 end
